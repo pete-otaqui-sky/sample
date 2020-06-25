0aac2f95b61b35830fa978670e74ae37e8c75b67
Pete Otaqui <pete.otaqui@sky.uk>
2020-06-25 18:34:07 +0100

    Tweaked cucumber capybara config

--- features/support/step_helpers.rb
+++ features/support/step_helpers.rb
@@ -1,8 +1,28 @@
 require 'capybara/cucumber'
 require 'capybara/rspec'
+require 'selenium-webdriver'
 
-Capybara.default_driver = :selenium_chrome_headless # :selenium_chrome and :selenium_chrome_headless are also registered
-Capybara.current_driver = :selenium_chrome_headless
+
+Capybara.register_driver :test do |app|
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  browser_options.args << '--headless'
+  browser_options.args << '--no-sandbox'
+  browser_options.args << '--disable-gpu'
+  browser_options.args << '--window-size=1920,1080'
+  browser_options.args << '--disable-dev-shm-usage'
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: browser_options
+  )
+end
+
+
+Capybara.configure do |config|
+  config.default_driver = :test
+  # Other irrelevant config stuff...
+end
+
 Capybara.run_server = false
 
 Capybara.app_host = 'http://www.google.com'
